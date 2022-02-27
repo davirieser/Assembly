095031adb985f96a9d8ac0da33c7e956da9f0725
David Rieser <david.w.rieser@gmail.com>
2022-02-27 17:10:39 +0100

    Created String Macro

--- hello_world/hello_world.asm
+++ hello_world/hello_world.asm
@@ -16,6 +16,20 @@ global _start
         mov rdx, msg_len
         syscall
 
+        ; ssize_t write(int fd, const void *buf, size_t count)
+        mov rax, SYS_WRITE
+        mov rdi, STDOUT
+        mov rsi, message2
+        mov rdx, message2_len
+        syscall
+
+        ; ssize_t write(int fd, const void *buf, size_t count)
+        mov rax, SYS_WRITE
+        mov rdi, STDOUT
+        mov rsi, message3
+        mov rdx, message3_len
+        syscall
+
         ; void _exit(int status)
         mov rax, SYS_EXIT
         mov rdi, 0
@@ -26,5 +40,7 @@ global _start
 section .data:
     message: db "Hello World!", 0xA
     msg_len: equ $ - message
+    STRING message2, {"Hello World!", 0xA}
+    STRING message3, `Hello World!\n`
 
 ; ---------------------------------------------------------------------------- ;
